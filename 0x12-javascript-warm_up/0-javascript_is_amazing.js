3bd03f437e0690b671eb0851f678e6bf0fc10eca
Sir-Aleem <aleemadesina@gmail.com>
2022-10-05 22:44:36 +0000

    my fresh javascript

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+/* Print a variable const */
+
+const myVar = 'JavaScript is amazing';
+
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,10 @@
+13-add.js
+#!/usr/bin/node
+
+// Function that returns the addition of 2 integers
+
+exports.add = function (a, b) {
+
+  return a + b;
+
+};
